849d196838161286bdee332ba338c4830a68b4ba
greenmac <alwaysmac@msn.com>
2019-01-14 22:20:04 +0800

    hide whitenoise

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -16,7 +16,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 # application = get_wsgi_application()
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
