c3961b8a00791801438e536af6f2ac4d5602c149
greenmac <alwaysmac@msn.com>
2019-01-16 14:49:38 +0800

    heroku setting

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -6,7 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-
+###----------
+##本機開發設定
 # import os
 
 # from django.core.wsgi import get_wsgi_application
@@ -15,8 +16,14 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 # application = get_wsgi_application()
 
-from django.core.wsgi import get_wsgi_application
-# from whitenoise.django import DjangoWhiteNoise
 
-application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
+###----------
+##heroku設定
+import os
+ 
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+ 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+ 
+application = Cling(get_wsgi_application())
